3280335138e1fa4a7ae14d792893c1f298e02346
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-06-19 12:05:35 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/home', methods=["GET", "POST"])
+def home():
+    if not request.args.get('id'):
+        frame = 'https://www.youtube.com/embed/rYAf_yClD8o'
+        heading = 'Lecture'
+    else:
+        video_num = int(request.args.get('id'))
+        video_headings = ['Primary Sector', 'Secondary Sector', 'Tertiary Sector']
+        videos = ["https://www.youtube.com/embed/0GJN5pyxWFQ", "https://www.youtube.com/embed/rYAf_yClD8o", "https://www.youtube.com/embed/lj4lrMudcIQ"]
+        for video in videos:
+            if video_num == videos.index(video) + 1:
+                frame = video
+                heading = video_headings[video_num - 1]
+    return render_template('index.html', video=frame, heading=heading)
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/password')
+def password():
+    return render_template('password.html')
+
+
+@app.route('/page_not_found')
+def page_not_found():
+    return render_template('404.html')
+
+
+@app.route('/unauthorized')
+def unauthorized():
+    return render_template('401.html')
+
+
+@app.route('/server_error')
+def server_error():
+    return render_template('500.html')
+
+
+@app.route('/charts')
+def charts():
+    return render_template('charts.html')
+
+
+@app.route('/tables')
+def tables():
+    return render_template('tables.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
