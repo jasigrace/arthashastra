b2b62a7f5d3762a1f838d4f729d5f45e26e8bb93
JASI GRACE I <67458870+jasigrace@users.noreply.github.com>
2021-06-24 14:50:53 +0530

    first commit

---
+++ static/js/quiz_questions/easy/quiz_11.js
@@ -0,0 +1,36 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the first step in cultivating a land?",
+    answer: "Ploughing",
+    options: [
+      "Sowing",
+      "Harvesting",
+      "Ploughing",
+      "Transportation"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Dry Farming grows only with?",
+    answer: "Rain water",
+    options: [
+      "Artificial watering",
+      "Rain water",
+      "Both a and b",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "In which farming plants are watered artificially?",
+    answer: "Irrigation farming",
+    options: [
+      "Irrigation farming",
+      "Dry farming",
+      "Both a and b",
+      "None of the above"
+    ]
+  }
+];
